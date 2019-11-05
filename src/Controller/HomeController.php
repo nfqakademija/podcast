4b9cd052ff1a174677bc33ae2e977aaa1d279ed8
Edvardas Petrauskas <admin@evoip.lt>
2019-11-05 14:39:22 +0200

    testing

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,7 +13,7 @@ class HomeController extends AbstractController
     public function index()
     {
         return $this->render('home/index.html.twig', [
-            'someVariable' => 'Krepšinio podcastai ! ??? ES test 2',
+            'someVariable' => 'Krepšinio podcastai',
         ]);
     }
 }
