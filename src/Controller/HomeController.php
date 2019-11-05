e504ae544668fea19f3144a0fcea39149609379e
Aurelijus Banelis <aurelijus@banelis.lt>
2019-11-05 13:17:44 +0200

    Testing deployment

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,7 +13,7 @@ class HomeController extends AbstractController
     public function index()
     {
         return $this->render('home/index.html.twig', [
-            'someVariable' => 'Krepšinio podcastai',
+            'someVariable' => 'Krepšinio podcastai', // Testing deployment
         ]);
     }
 }
