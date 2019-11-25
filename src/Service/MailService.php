1e33076af336c13faa505de5113969bde683c656
Kazimieras Babrauskas <b.kazimieras17@gmail.com>
2019-11-25 22:18:08 +0200

    Created notification sending method

--- src/Service/MailService.php
+++ src/Service/MailService.php
@@ -3,15 +3,18 @@
 
 namespace App\Service;
 
+use App\Entity\Podcast;
 use App\Entity\Subscriber;
 use App\Repository\SubscriberRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
 use Swift_Mailer;
+use Swift_Message;
+use Throwable;
 use Twig\Environment;
-use Twig\Error\LoaderError;
-use Twig\Error\RuntimeError;
-use Twig\Error\SyntaxError;
+
+const SENDERS_EMAIL = 'krepsinio.podcast@gmail.com';
+
 
 class MailService
 {
@@ -19,6 +22,7 @@ class MailService
     private $templating;
     private $logger;
     private $subscriberRepository;
+    private $entityManager;
 
     public function __construct(
         Swift_Mailer $mailer,
@@ -34,12 +38,12 @@ class MailService
         $this->subscriberRepository = $subscriberRepository;
     }
 
-    public function sendVerification($email)
+    public function sendVerification($email): bool
     {
         try {
-            $message = (new \Swift_Message())
+            $message = (new Swift_Message())
                 ->setSubject('Prenumeratos patvirtinimas || Krepšinio Podkastai')
-                ->setFrom('krepsinio.podcast@gmail.com')
+                ->setFrom(SENDERS_EMAIL)
                 ->setTo($email)
                 ->setBody(
                     $this->templating->render(
@@ -48,13 +52,7 @@ class MailService
                     ),
                     'text/html'
                 );
-        } catch (LoaderError $e) {
-            $this->logger->error($e->getMessage());
-            return false;
-        } catch (RuntimeError $e) {
-            $this->logger->error($e->getMessage());
-            return false;
-        } catch (SyntaxError $e) {
+        } catch (Throwable $e) {
             $this->logger->error($e->getMessage());
             return false;
         }
@@ -65,10 +63,53 @@ class MailService
 
             $this->entityManager->persist($subscriber);
             $this->entityManager->flush();
+            return true;
         }
+
+        return false;
     }
+
     private function checkIfEmailExists($email): bool
     {
         return empty($this->subscriberRepository->findOneBy(['email' => $email]))?true:false;
+    }
+
+    /**
+     * @var Podcast $podcast
+     */
+    public function sendNotification($podcast): bool
+    {
+        $subscribers = $this->subscriberRepository->findBy([
+            'isConfirmed' => true,
+        ]);
+
+        /** @var Subscriber $subscriber */
+        foreach ($subscribers as $subscriber) {
+            try {
+                $message = (new Swift_Message())
+                    ->setSubject('Naujas įrašas|| Krepšinio Podkastai')
+                    ->setFrom(SENDERS_EMAIL)
+                    ->setTo($subscriber->getEmail())
+                    ->setBody(
+                        $this->templating->render(
+                            'emails/subscriberNotification.html.twig',
+                            [
+                                'email' => $subscriber->getEmail(),
+                                'podcast' => $podcast,
+                            ]
+                        ),
+                        'text/html'
+                    );
+            } catch (Throwable $e) {
+                $this->logger->error($e);
+                return false;
+            }
+
+            if ($this->mailer->send($message)) {
+                return true;
+            }
+
+            return false;
+        }
     }
 }
