7ec7f0903d94c90c78bbdcaabb91d71516a0e1d0
Kazimieras Babrauskas <b.kazimieras17@gmail.com>
2019-11-27 20:54:43 +0200

    Exploded YoutubeService importDataFromYoutube method to smallers methods.

--- src/Service/YoutubeService.php
+++ src/Service/YoutubeService.php
@@ -13,11 +13,7 @@ use Exception;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpClient\HttpClient;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
-use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
-use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
-use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
-use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+use Throwable;
 
 class YoutubeService
 {
@@ -47,117 +43,110 @@ class YoutubeService
         $this->apiCode = $apiCode;
     }
 
-    public function importDataFromYoutube(array &$listOfSources): bool
+    public function importDataFromYoutube(array $listOfSources): bool
     {
         /** @var Source $source */
         foreach ($listOfSources as $source) {
             $playlist = false;
             if (preg_match('~playlist\?list=~', $source->getUrl())) {
-                $parts = parse_url($source->getUrl());
-                parse_str($parts['query'], $query);
-                try {
-                    $response = $this->httpClient->request('GET', $this->requestUrl . 'playlistItems', [
-                        'query' => [
-                            'part' => 'snippet',
-                            'playlistId' => $query['list'],
-                            'maxResults' => '50',
-                            'pageToken' => '',
-                            'key' => $this->apiCode
-                        ]
-                    ]);
-                    $playlist = true;
-                } catch (TransportExceptionInterface $e) {
-                    $this->logger->error($e->getMessage());
-                    return false;
-                }
+                $content = $this->getDataFromPlaylist($source->getUrl());
+                $playlist = true;
             } else {
-                $channelId = $this->getChannelId($source);
-                try {
-                    $response = $this->httpClient->request('GET', $this->requestUrl . 'search', [
-                        'query' => [
-                            'part' => 'snippet',
-                            'channelId' => $channelId,
-                            'maxResults' => '50',
-                            'order' => 'date',
-                            'pageToken' => '',
-                            'type' => 'video',
-                            'key' => $this->apiCode
-                        ]
-                    ]);
-                } catch (TransportExceptionInterface $e) {
-                    $this->logger->error($e->getMessage());
-                    return false;
-                }
+                $content = $this->getDataFromChannel($source);
             }
 
             try {
-                $content = $response->toArray();
-                if ($response->getStatusCode() === 200) {
-                    foreach ($content['items'] as $video) {
-                        if ($playlist) {
-                            $videoId = explode('/', end($video['snippet']['thumbnails'])['url']);
-                        }
-                        if ((empty($video['snippet']['liveBroadcastContent'])?
-                                true
-                                :
-                                $video['snippet']['liveBroadcastContent'] === 'none')
-                            && !$this->isVideoExists((
-                            $playlist?
-                                $videoId[sizeof($videoId)-2]
-                                :
-                                $video['id']['videoId']))
-                        ) {
-                            $podcast = new Podcast();
-
-                            $podcast->setSource($source);
-                            $podcast->setPublishedAt(new DateTime($video['snippet']['publishedAt']));
-                            $podcast->setDescription($video['snippet']['description']);
-                            $podcast->setTitle($video['snippet']['title']);
-                            $podcast->setImage(end($video['snippet']['thumbnails'])['url']);
-                            $podcast->setCreatedAt(new DateTime('now'));
-                            if ($playlist) {
-                                $podcast->setVideo($videoId[sizeof($videoId)-2]);
-                            } else {
-                                $podcast->setVideo($video['id']['videoId']);
-                            }
-                            $this->entityManager->persist($podcast);
-                            $this->entityManager->flush();
-                        }
+                foreach ($content['items'] as $video) {
+                    if ($playlist) {
+                        $videoId = explode('/', end($video['snippet']['thumbnails'])['url']);
+                        $videoId = $videoId[sizeof($videoId)-2];
                     }
-                } else {
-                    $this->logger->error($content['error']['message']);
+                    if (($video['snippet']['liveBroadcastContent']??'none') === 'none'
+                        && !$this->isVideoExists($videoId??$video['id']['videoId'])
+                    ) {
+                        $podcast = new Podcast();
+
+                        $podcast->setSource($source);
+                        $podcast->setPublishedAt(new DateTime($video['snippet']['publishedAt']));
+                        $podcast->setDescription($video['snippet']['description']);
+                        $podcast->setTitle($video['snippet']['title']);
+                        $podcast->setImage(end($video['snippet']['thumbnails'])['url']);
+                        $podcast->setCreatedAt(new DateTime('now'));
+                        $podcast->setVideo($videoId??$video['id']['videoId']);
+
+                        $this->entityManager->persist($podcast);
+                        $this->entityManager->flush();
+                    }
                 }
-            } catch (ClientExceptionInterface $e) {
-                $this->logger->error($e->getMessage());
-                return false;
-            } catch (DecodingExceptionInterface $e) {
-                $this->logger->error($e->getMessage());
-                return false;
-            } catch (RedirectionExceptionInterface $e) {
-                $this->logger->error($e->getMessage());
-                return false;
-            } catch (ServerExceptionInterface $e) {
-                $this->logger->error($e->getMessage());
-                return false;
-            } catch (TransportExceptionInterface $e) {
-                $this->logger->error($e->getMessage());
-                return false;
-            } catch (Exception $e) {
-                $this->logger->error($e->getMessage());
+
+            } catch (Throwable $e) {
+                $this->logger->error($e);
                 return false;
             }
         }
         return true;
     }
 
+    private function getDataFromPlaylist($url): array
+    {
+        $parts = parse_url($url);
+        parse_str($parts['query'], $query);
+        try {
+            $response = $this->httpClient->request('GET', $this->requestUrl . 'playlistItems', [
+                'query' => [
+                    'part' => 'snippet',
+                    'playlistId' => $query['list'],
+                    'maxResults' => '50',
+                    'pageToken' => '',
+                    'key' => $this->apiCode
+                ]
+            ]);
+            $content = $response->toArray();
+
+            if ($response->getStatusCode() != 200) {
+                throw new Exception($content['error']['message']);
+            }
+            return $content;
+        } catch (Throwable $e) {
+            $this->logger->error($e);
+            return [];
+        }
+    }
+
+    private function getDataFromChannel(Source $source): array
+    {
+        $channelId = $this->getChannelId($source);
+        try {
+            $response = $this->httpClient->request('GET', $this->requestUrl . 'search', [
+                'query' => [
+                    'part' => 'snippet',
+                    'channelId' => $channelId,
+                    'maxResults' => '50',
+                    'order' => 'date',
+                    'pageToken' => '',
+                    'type' => 'video',
+                    'key' => $this->apiCode
+                ]
+            ]);
+            $content = $response->toArray();
+            if ($response->getStatusCode() != 200) {
+                throw new Exception($content['error']['message']);
+            }
+            return $content;
+        } catch (Throwable $e) {
+            $this->logger->error($e);
+            return [];
+        }
+    }
+
     private function isVideoExists($videoId): bool
     {
-        if (!empty($this->podcastRepository->findOneBy([
+        if (empty($this->podcastRepository->findOneBy([
             'video' => $videoId
         ]))) {
-            return true;
-        } else {
             return false;
+        } else {
+            return true;
         }
     }
 
@@ -168,7 +157,7 @@ class YoutubeService
             $channelId =  $this->getChannelIdByUser(end($sourceExploded));
             if (!empty($channelId)) {
                 $source->setUrl('https://www.youtube.com/channel/'.$channelId);
-                $this->entityManager->merge($source);
+                $this->entityManager->persist($source);
                 $this->entityManager->flush();
 
                 return $channelId;
@@ -191,33 +180,16 @@ class YoutubeService
                     'key' => $this->apiCode
                 ]
             ]);
-        } catch (TransportExceptionInterface $e) {
-            $this->logger->error($e->getMessage());
-            return false;
-        }
 
-        try {
-            if ($response->getStatusCode() === 200) {
-                $content = $response->toArray();
-                return $content['items'][0]['id'];
+            $content = $response->toArray();
+
+            if ($response->getStatusCode() != 200) {
+                throw new Exception($content['error']['message']);
             }
-        } catch (ClientExceptionInterface $e) {
-            $this->logger->error($e->getMessage());
-            return false;
-        } catch (DecodingExceptionInterface $e) {
-            $this->logger->error($e->getMessage());
-            return false;
-        } catch (RedirectionExceptionInterface $e) {
-            $this->logger->error($e->getMessage());
-            return false;
-        } catch (ServerExceptionInterface $e) {
-            $this->logger->error($e->getMessage());
-            return false;
-        } catch (TransportExceptionInterface $e) {
-            $this->logger->error($e->getMessage());
-            return false;
-        } catch (Exception $e) {
-            $this->logger->error($e->getMessage());
+            return $content['items'][0]['id'];
+
+        } catch (Throwable $e) {
+            $this->logger->error($e);
             return false;
         }
     }
