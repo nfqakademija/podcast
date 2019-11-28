87cff87acd18b3131e66782d84450a90aad77f86
Nerijus <nerijuspamerneckis@gmail.com>
2019-11-29 01:04:55 +0200

    implemented daily subscription mailing and unsubscribe

--- src/Controller/SubscriberController.php
+++ src/Controller/SubscriberController.php
@@ -4,37 +4,47 @@ namespace App\Controller;
 
 use App\Entity\Subscriber;
 use App\Form\SubscriberType;
+use App\Service\MailService;
 use App\Service\TokenGenerator;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SubscriberController extends AbstractController
 {
     private $entityManager;
 
-    public function __construct(EntityManagerInterface $entityManager)
+    private $tokenGenerator;
+
+    public function __construct(EntityManagerInterface $entityManager, TokenGenerator $tokenGenerator)
     {
         $this->entityManager = $entityManager;
+        $this->tokenGenerator = $tokenGenerator;
     }
 
     /**
      * @Route("subscribe", name="new_subscriber")
      */
-    public function createSubscriber(
-        Request $request,
-        TokenGenerator $tokenGenerator
-    ) {
+    public function createSubscriber(Request $request)
+    {
         $form = $this->createForm(SubscriberType::class, null, [
             'action' => $this->generateUrl('new_subscriber')
         ]);
         $form->handleRequest($request);
 
+        if ($form->isSubmitted() && !$form->isValid())
+        {
+            $this->addFlash('danger', 'Toks prenumeratorius jau egzistuoja!');
+
+            return $this->redirectToRoute('podcasts');
+        }
+
         if ($form->isSubmitted() && $form->isValid()) {
             /** @var Subscriber $subscriber */
             $subscriber = $form->getData();
-            $subscriber->setConfirmationToken($tokenGenerator->getRandomSecureToken(100));
+            $subscriber->setConfirmationToken($this->tokenGenerator->getRandomSecureToken(100));
 
             $this->entityManager->persist($subscriber);
             $this->entityManager->flush();
@@ -57,6 +67,7 @@ class SubscriberController extends AbstractController
         if ($subscriber) {
             $subscriber->setIsConfirmed(true);
             $subscriber->setConfirmationToken(null);
+            $subscriber->setUnsubscribeToken($this->tokenGenerator->getRandomSecureToken(100));
 
             $this->entityManager->flush();
 
@@ -65,6 +76,31 @@ class SubscriberController extends AbstractController
             ]);
         }
 
-        $this->createNotFoundException();
+        return $this->createNotFoundException();
+    }
+
+    /**
+     * @Route("unsubscribe/{unsubscribeToken}", name="unsubscribe")
+     */
+    public function deleteSubscriber(Subscriber $subscriber)
+    {
+        if ($subscriber) {
+            $this->entityManager->remove($subscriber);
+            $this->entityManager->flush();
+
+            return $this->render('emails/unsubscribe.html.twig');
+        }
+
+        return $this->createNotFoundException();
+    }
+
+    /**
+     * @Route("daily-newsletter", name="daily_newsletter")
+     */
+    public function sendDailyNewsletter(MailService $mailService)
+    {
+        $mailService->sendDailyNewsletterToSubscribers();
+
+        return new Response('sent');
     }
 }
