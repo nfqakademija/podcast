fb048aef552285df70ccb21475dd4a9236fb77df
Nerijus <nerijuspamerneckis@gmail.com>
2019-12-05 19:53:03 +0200

    added user_tag relation

--- src/Entity/Tag.php
+++ src/Entity/Tag.php
@@ -33,9 +33,15 @@ class Tag
      */
     private $podcasts;
 
+    /**
+     * @ORM\ManyToMany(targetEntity="App\Entity\User", mappedBy="tags")
+     */
+    private $users;
+
     public function __construct()
     {
         $this->podcasts = new ArrayCollection();
+        $this->users = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -84,5 +90,33 @@ class Tag
     public function __toString()
     {
         return $this->tag;
+    }
+
+    /**
+     * @return Collection|User[]
+     */
+    public function getUsers(): Collection
+    {
+        return $this->users;
+    }
+
+    public function addUser(User $user): self
+    {
+        if (!$this->users->contains($user)) {
+            $this->users[] = $user;
+            $user->addTag($this);
+        }
+
+        return $this;
+    }
+
+    public function removeUser(User $user): self
+    {
+        if ($this->users->contains($user)) {
+            $this->users->removeElement($user);
+            $user->removeTag($this);
+        }
+
+        return $this;
     }
 }
